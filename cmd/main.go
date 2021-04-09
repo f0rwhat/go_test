3aa746e70b12b59a6cd9935a21affe5d2536e470
f0rwhat <45487627+f0rwhat@users.noreply.github.com>
2021-04-09 09:58:39 +0700

    добавлен рабочий пример, добавлен health check

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,102 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"math/rand"
 	"net/http"
-	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	http.HandleFunc("/hello-world", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprintf(w, "Hello world")
-	})
+type Book struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
 
-	port, exists := os.LookupEnv("PORT")
-	if exists == false {
-		fmt.Printf("NO PORT")
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
 	}
-	http.ListenAndServe(":"+port, nil)
+	json.NewEncoder(w).Encode(&Book{})
+}
 
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	r := mux.NewRouter()
+	books = append(books, Book{ID: "1", Title: "Война и Мир", Author: &Author{Firstname: "Лев", Lastname: "Толстой"}})
+	books = append(books, Book{ID: "2", Title: "Преступление и наказание", Author: &Author{Firstname: "Фёдор", Lastname: "Достоевский"}})
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+	r.HandleFunc("/health", healthCheck).Methods("HEAD")
+	log.Fatal(http.ListenAndServe(":8000", r))
+	//port, exists := os.LookupEnv("PORT")
+	//if exists == false {
+	//	fmt.Printf("NO PORT")
+	//}
+
+	//http.ListenAndServe(":"+port, nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-test
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0
